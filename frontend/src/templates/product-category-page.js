c455e7cad835a2519b55f350f1c1e434965df255
Nathan Tsai <nathan@albernidesign.com>
2021-10-23 02:03:15 -0700

    Contineu building product-category-page

---
+++ frontend/src/templates/product-category-page.js
@@ -0,0 +1,75 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import Img from "gatsby-image"
+
+
+const ProductCategories = ({ data }) => {
+  console.log(data);
+  const taxonomyTerm = data.taxonomyTermProductCategories;
+  const products = data.allNodeProduct.edges;
+        
+  // const rows = 1;
+  // const cols = 2;
+
+  return (
+    <Layout>
+      <div>
+        <h1>{ taxonomyTerm.name }</h1>
+
+        <ImageList>
+          {products.map(({ node: product }) => (
+            <ImageListItem key={product.id}>
+              <div style={{ maxWidth: `250px`, marginBottom: `1rem` }}>
+                <Img fixed={ product.relationships.field_image.localFile.childImageSharp.fixed } />
+              </div>
+              <ImageListItemBar
+                title={product.title}
+                subtitle={<span>Before BBD</span>}
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+
+      </div>
+    </Layout>
+  )
+}
+export default ProductCategories;
+
+export const query = graphql`
+  query ($tid: String!) {
+    allNodeProduct(
+      filter: {relationships: {field_categories: {elemMatch: {id: {eq: $tid}}}}}
+    ) {
+      edges {
+        node {
+          id
+          title
+          relationships {
+            field_image {
+              localFile {
+                absolutePath
+                childImageSharp {
+                  fluid(cropFocus: NORTH) {
+                    ...GatsbyImageSharpFluid
+                  }
+                  fixed(cropFocus: NORTH, width: 250, height: 180) {
+                    ...GatsbyImageSharpFixed
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    taxonomyTermProductCategories(id: {eq: $tid}) {
+      id
+      name
+    }
+  }
+`;
