28b06af80826fa141d32a8eda8195cab7c5e8236
Alison Wu <alison.wu.0606@gmail.com>
2021-10-22 23:27:01 -0700

    Sticky footer

---
+++ frontend/src/components/footer.js
@@ -0,0 +1,41 @@
+import * as React from "react"
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import HelpIcon from '@mui/icons-material/Help';
+
+const Footer = () => (
+  <footer style={{
+      backgroundColor: 'gray',
+      padding: '1rem 2rem 0rem 2rem',
+      position: 'fixed',
+      bottom: '0',
+      left: '0',
+      width: '100%'
+    }}>
+    <Box sx={{
+        flexGrow: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        color: 'white'
+      }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <StorefrontIcon />
+          <p>Home</p>
+        </Grid>
+        <Grid item xs={4}>
+          <AccountCircleIcon />
+          <p>Account</p>
+        </Grid>
+        <Grid item xs={4}>
+          <HelpIcon />
+          <p>Help</p>
+        </Grid>
+      </Grid>
+    </Box>
+  </footer>
+)
+
+export default Footer
