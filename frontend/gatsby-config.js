5058b26db0f4e5264686c80e5767faeacdaf1318
Alison Wu <alison.wu.0606@gmail.com>
2021-10-22 21:51:33 -0700

    Update some MUI imports, starting homepage

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Omeed Grocer`,
+    description: `House of Omeed - Online Grocery Store`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,30 +1,14 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-
-import Button from '@material-ui/core/Button';
+import CategoryGrid from "../components/category-grid"
 
 const HomePage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Button variant="contained">Hello World</Button>
-      <Link to="/home/">Go to home again</Link> <br />
-    </p>
+    <h1>Start Shopping</h1>
+    <CategoryGrid/>
   </Layout>
 )
 
