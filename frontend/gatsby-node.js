9d00f7a5c0b4114973553b7ab78a05047f98c763
Nathan Tsai <nathan@albernidesign.com>
2021-10-22 23:29:22 -0700

    Begin building categories

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -1,9 +1,32 @@
-exports.createPages = async ({ actions }) => {
-  const { createPage } = actions
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
+exports.createPages = async ({ graphql, actions }) => {
+  // const { createPage } = actions
+  // createPage({
+  //   path: "/using-dsg",
+  //   component: require.resolve("./src/templates/using-dsg.js"),
+  //   context: {},
+  //   defer: true,
+  // })
+  return graphql(`
+    {
+      allTaxonomyTermProductCategories {
+       edges {
+         node {
+           id
+           name
+         }
+       }
+     }
+    }
+  `
+  ).then(result => {
+    result.data.allNodeArticle.edges.forEach(({ categories }) => {
+      createPage({
+        path: `categories/{categories.id}`,
+        component: path.resolve(`./src/templates/product-category-page.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
   })
 }
