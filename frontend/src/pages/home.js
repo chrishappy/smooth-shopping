cc990e92c5b721e99b4b61323988618ec170fdc1
Alison Wu <alison.wu.0606@gmail.com>
2021-10-22 20:23:21 -0700

    Add gatsby starter

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,31 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+import Button from '@material-ui/core/Button';
+
+const HomePage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/gatsby-astronaut.png"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+    <p>
+      <Button variant="contained">Hello World</Button>
+      <Link to="/home/">Go to home again</Link> <br />
+    </p>
+  </Layout>
+)
+
+export default HomePage
