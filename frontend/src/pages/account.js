b2636c8baa796fac3f3adfa22263bce9c0683198
Nathan Tsai <nathan@albernidesign.com>
2021-10-24 02:38:36 -0700

    Add account page

---
+++ frontend/src/pages/account.js
@@ -0,0 +1,48 @@
+import * as React from "react"
+
+import Seo from "../components/seo"
+import Typography from '@mui/material/Typography';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';// import CardMedia from '@mui/material/CardMedia';
+import { connect } from 'react-redux';
+
+const Account = ({  appState }) => {
+
+  const userData = appState.user;
+  return (
+    <>
+      <Seo title="Cart Page" />
+      <h1 style={{ textAlign: 'center', marginBottom: '1em' }}>Your Account</h1>
+
+      <h3>{ userData.familyName }</h3>
+
+      <Typography component="div" variant="body1">
+
+      <p>Number of Family Members: {userData.numberOfFamilyMembers}</p>
+
+      <hr></hr>
+
+      <p>
+        <MonetizationOnOutlinedIcon sx={{ verticalAlign: 'top' }} /> 
+        <strong> Monthly Plan</strong>: ${userData.monthlyAllowance} <Typography component="span" variant="body2">/month</Typography>
+      </p>
+
+      <p>
+        <MonetizationOnOutlinedIcon sx={{ verticalAlign: 'top' }} /> 
+        <strong> Current Balance:</strong> ${userData.creditsRemaining} <Typography component="span" variant="body2">/{userData.totalCredits} credits</Typography>
+      </p>
+      
+      <hr></hr>
+      </Typography>
+
+      <Typography component="div" color="text.secondary"  variant="body2">
+        Your credits are renewed on the first of every month. Credits do not roll over.
+      </Typography>
+    </>
+  );
+}
+
+export default connect(state => ({
+  appState: state
+}), dispatch => ({
+  storeDispatch: dispatch
+}))(Account)
