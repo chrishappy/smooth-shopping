d4e87082f503e0cb40932431067662dd85071028
Nathan Tsai <nathan@albernidesign.com>
2021-10-24 12:53:37 -0700

    Add checkout component

---
+++ frontend/src/components/checkout.js
@@ -0,0 +1,72 @@
+import * as React from "react"
+import { navigate } from "gatsby";
+import { connect } from "react-redux";
+
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import Badge from '@mui/material/Badge';
+import { styled } from '@mui/material/styles';
+import { Fab } from "@mui/material";
+import { Box } from "@mui/system";
+
+const CheckoutButton = ({ appState }) => {
+
+  const cartItems = appState.cartItems;
+
+  const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      right: 4,
+      top: 5,
+      padding: '0 4px',
+      backgroundColor: '#eee',
+      color: '#000',
+      fontWeight: 'bold',
+    },
+  }));
+
+
+  return (
+    <Box
+    sx={{
+      position: 'fixed',
+      maxWidth: 400,
+      left: 0,
+      right: 0,
+      margin: '0 auto',
+      bottom: '5rem',
+      top: 0,
+    }}>
+      <Fab color="primary"
+          aria-label="Checkout your items"
+          sx={{
+            color: '#000',
+            backgroundColor: '#75F348',
+            position: 'absolute',
+            bottom: '1em',
+            right: '1em',
+          }}
+          size="large">
+          <StyledBadge badgeContent={cartItems.numberOfProducts} color="primary">
+            <ShoppingCartOutlinedIcon  className="checkout__icon" sx={{ verticalAlign: 'top'}} />
+          </StyledBadge>
+      </Fab>
+    </Box>
+    // <Box>
+    //   <Button
+    //     sx={{ color: '#000', background: '#75F348' }}
+    //     aria-label="Checkout your items"
+    //     onClick={() => navigate(-1)}>
+    //       <Badge badgeContent={cartItems.numberOfProducts} color="primary">
+    //         <ShoppingCartOutlinedIcon  className="checkout__icon" sx={{ verticalAlign: 'top'}} />
+    //       </Badge>
+
+    //       {/* <ChevronRightIcon className="checkout__icon--right" /> */}
+    //   </Button>
+
+    //   <strong className="checkout__text">Checkout</strong>
+    // </Box>
+  );
+}
+
+export default connect(state => ({
+  appState: state
+}), null)(CheckoutButton)
--- frontend/src/templates/product-category-page.js
+++ frontend/src/templates/product-category-page.js
@@ -40,104 +40,100 @@ const ProductCategories = ({ data, appState, storeDispatch }) => {
 
   return (
     <>
-      <div>
-        <h1>{ taxonomyTerm.name }</h1>
+      <h1>{ taxonomyTerm.name }</h1>
 
-        <ImageList
-          sx={{ margin: '0' }}
-          className="product-listings"
-          gap="8px">
-          {products.map(({ node: product }) => (
-            <Box key={product.id} onClick={() => {
-              setProduct(product);
-              setOpen(true);
-            }}  className="product-listing">
-              <Img fluid={ product.relationships.field_image.localFile.childImageSharp.fluid } />
-              <Box className="product-listing__content">
-                <h3 className="product-listing__title">{product.title}</h3>
-                <Box sx={{ textAlign: 'right', }}>
-                {product.field_expired_ ? <WarningAmberIcon sx={{verticalAlign: 'top', color: '#FA9500' }}/> : ''} ${product.field_credit}
-                </Box>
+      <ImageList
+        sx={{ margin: '0' }}
+        className="product-listings"
+        gap="8px">
+        {products.map(({ node: product }) => (
+          <Box key={product.id} onClick={() => {
+            setProduct(product);
+            setOpen(true);
+          }}  className="product-listing">
+            <Img fluid={ product.relationships.field_image.localFile.childImageSharp.fluid } />
+            <Box className="product-listing__content">
+              <h3 className="product-listing__title">{product.title}</h3>
+              <Box sx={{ textAlign: 'right', }}>
+              {product.field_expired_ ? <WarningAmberIcon sx={{verticalAlign: 'top', color: '#FA9500' }}/> : ''} ${product.field_credit}
               </Box>
             </Box>
-          ))}
-        </ImageList>
+          </Box>
+        ))}
+      </ImageList>
 
-        <Box>
-          <CheckoutButton />
-        </Box>
-        <Dialog
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <DialogContent sx={{ minWidth: '25rem' }}>
-          <Stack>
-            {selectedProduct.hasOwnProperty('relationships') ?
-              <Img fluid={ selectedProduct.relationships.field_image.localFile.childImageSharp.fluid }/>
-              : <img src="https://source.unsplash.com/random" alt="random item"/>
-            }
-            <Box sx={{ margin: '0.5rem 0 1rem' }}>
-              <Typography id="modal-product-title" variant="h6" component="h2" sx={{ mt: 0.5, fontWeight: 'bold' }}>
-                {selectedProduct.title}
-              </Typography>
-              <Typography id="modal-product-description" sx={{ mb: 1 }}>
-                <p>
-                  {selectedProduct.field_expired_
-                    ? <span><WarningAmberIcon sx={{verticalAlign: 'top', color: '#FA9500' }}/>Expired</span>
-                    : <span>Not expired</span>}
-                </p>
-              </Typography>
-            </Box>
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-              <Stack direction="row">
-                <IconButton
-                  style={mathButtonStyle}
-                  onClick={() => {
-                    let count = (selectedProductCount-1 < 0) ? 1 : selectedProductCount-1;
-                    setCount(count);
-                  }}
-                  >
-                  <RemoveIcon />
-                </IconButton>
-                <Typography id="modal-product-count" sx={{ mt:1, ml:0.5, mr:0.5 }}>
-                  {selectedProductCount}
-                </Typography>
-                <IconButton
-                  style={mathButtonStyle}
-                  onClick={() => {
-                    setCount(selectedProductCount + 1);
-                  }}
-                  >
-                  <AddIcon />
-                </IconButton>
-              </Stack>
-              <div style={{marginRight: 'auto'}}></div>
-              <Button variant="contained"
-                sx={{
-                  backgroundColor: '#75F348',
-                  color: 'black',
-                  borderRadius: '20px',
-                  fontWeight: 'bold',
-                  padding: '0 10%'
-                }}
+      <CheckoutButton />
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <DialogContent sx={{ minWidth: '25rem' }}>
+        <Stack>
+          {selectedProduct.hasOwnProperty('relationships') ?
+            <Img fluid={ selectedProduct.relationships.field_image.localFile.childImageSharp.fluid }/>
+            : <img src="https://source.unsplash.com/random" alt="random item"/>
+          }
+          <Box sx={{ margin: '0.5rem 0 1rem' }}>
+            <Typography id="modal-product-title" variant="h6" component="h2" sx={{ mt: 0.5, fontWeight: 'bold' }}>
+              {selectedProduct.title}
+            </Typography>
+            <Typography id="modal-product-description" sx={{ mb: 1 }}>
+              <p>
+                {selectedProduct.field_expired_
+                  ? <span><WarningAmberIcon sx={{verticalAlign: 'top', color: '#FA9500' }}/>Expired</span>
+                  : <span>Not expired</span>}
+              </p>
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Stack direction="row">
+              <IconButton
+                style={mathButtonStyle}
                 onClick={() => {
-                  storeDispatch({
-                    type: 'incrementProduct',
-                    product: selectedProduct,
-                    by: selectedProductCount
-                  });
-                  handleClose();
-                }}>Add to cart</Button>
-            </Box>
-          </Stack>
-          </DialogContent>
-        </Dialog>
+                  let count = (selectedProductCount-1 < 0) ? 1 : selectedProductCount-1;
+                  setCount(count);
+                }}
+                >
+                <RemoveIcon />
+              </IconButton>
+              <Typography id="modal-product-count" sx={{ mt:1, ml:0.5, mr:0.5 }}>
+                {selectedProductCount}
+              </Typography>
+              <IconButton
+                style={mathButtonStyle}
+                onClick={() => {
+                  setCount(selectedProductCount + 1);
+                }}
+                >
+                <AddIcon />
+              </IconButton>
+            </Stack>
+            <div style={{marginRight: 'auto'}}></div>
+            <Button variant="contained"
+              sx={{
+                backgroundColor: '#75F348',
+                color: 'black',
+                borderRadius: '20px',
+                fontWeight: 'bold',
+                padding: '0 10%'
+              }}
+              onClick={() => {
+                storeDispatch({
+                  type: 'incrementProduct',
+                  product: selectedProduct,
+                  by: selectedProductCount
+                });
+                handleClose();
+              }}>Add to cart</Button>
+          </Box>
+        </Stack>
+        </DialogContent>
+      </Dialog>
 
-        { products.length === 0 ? (<p>There are currently no products.</p>) : ''}
-
-      </div>
+      { products.length === 0 ? (<p>There are currently no products.</p>) : ''}
     </>
   )
 }
